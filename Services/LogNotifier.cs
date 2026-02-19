fe06730e2a7976a1ea4309d6f100c29d89be8d28
daniel <ddelga12@charlotte.edu>
2026-02-18 23:04:24 -0500

    added skeleton of LogNotifier and SystemClock method implementations

--- Services/LogNotifier.cs
+++ Services/LogNotifier.cs
@@ -1,6 +1,17 @@
+using ITCS3112_Lab1.Contracts;
+using ITCS3112_Lab1.Domain;
+
 namespace ITCS3112_Lab1.Services;
 
-public class LogNotifier
+public class LogNotifier : INotifier
 {
-    
+    public void DueSoon(Borrower borrower, CheckoutRecord record)
+    {
+        throw new NotImplementedException();
+    }
+
+    public void Overdue(Borrower borrower, CheckoutRecord record)
+    {
+        throw new NotImplementedException();
+    }
 }
--- Services/SystemClock.cs
+++ Services/SystemClock.cs
@@ -1,6 +1,11 @@
+using ITCS3112_Lab1.Contracts;
+
 namespace ITCS3112_Lab1.Services;
 
-public class SystemClock
+public class SystemClock : IClock
 {
-    
+    public DateTime Today()
+    {
+        throw new NotImplementedException();
+    }
 }
