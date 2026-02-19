6e115c6e4e4071fd8d1f8c4fef1598f5aa565b0b
daniel <ddelga12@charlotte.edu>
2026-02-18 23:11:35 -0500

    added the ICheckoutService interface

---
+++ Contracts/ICheckoutService.cs
@@ -0,0 +1,14 @@
+using ITCS3112_Lab1.Domain;
+
+namespace ITCS3112_Lab1.Contracts;
+
+public interface ICheckoutService
+{
+    ICatalog GetCatalog();
+    Receipt Checkout(string itemId, Borrower borrower, DateTime dueDate);
+    Receipt ReturnItem(string itemId);
+    void MarkLost(string itemId);
+    List<CheckoutRecord> ListActiveLoans();
+    List<CheckoutRecord> FindDueSoon(TimeSpan window);
+    List<CheckoutRecord> FindOverdue();
+}
