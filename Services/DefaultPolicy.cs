32ba90544e1ec075a751b2514f1bbb57057f706e
daniel <ddelga12@charlotte.edu>
2026-02-18 23:01:23 -0500

    added skeleton of DefaultPolicy method implementations

--- Services/DefaultPolicy.cs
+++ Services/DefaultPolicy.cs
@@ -1,6 +1,17 @@
+using ITCS3112_Lab1.Contracts;
+using ITCS3112_Lab1.Domain;
+
 namespace ITCS3112_Lab1.Services;
 
-public class DefaultPolicy
+public class DefaultPolicy : IPolicy
 {
-    
+    public bool CanCheckout(Item item)
+    {
+        throw new NotImplementedException();
+    }
+
+    public DateTime NormalizeDueDate(DateTime proposed)
+    {
+        throw new NotImplementedException();
+    }
 }
