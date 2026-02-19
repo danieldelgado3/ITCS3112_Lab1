7ccf6ae80018aba730203729f41172f7a2fa7657
daniel <ddelga12@charlotte.edu>
2026-02-18 23:00:27 -0500

    added skeleton of DefaultCatalog method implementations

--- Services/DefaultCatalog.cs
+++ Services/DefaultCatalog.cs
@@ -1,6 +1,29 @@
+using ITCS3112_Lab1.Contracts;
+using ITCS3112_Lab1.Domain;
+
 namespace ITCS3112_Lab1.Services;
 
-public class DefaultCatalog
+public class DefaultCatalog : ICatalog
 {
-    
+    private readonly IRepository _repository;
+
+    public DefaultCatalog(IRepository repository)
+    {
+        this._repository = repository;
+    }
+
+    public List<Item> ListAvailable()
+    {
+        throw new NotImplementedException();
+    }
+
+    public Item? FindById(string itemId)
+    {
+        throw new NotImplementedException();
+    }
+
+    public List<Item> SearchBy(string query)
+    {
+        throw new NotImplementedException();
+    }
 }
