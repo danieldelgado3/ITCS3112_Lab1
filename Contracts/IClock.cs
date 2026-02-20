1df2255935802201aa0b7c7d1af784ca664ef3e9
daniel <ddelga12@charlotte.edu>
2026-02-20 00:57:56 -0500

    added xml for iclock

--- Contracts/IClock.cs
+++ Contracts/IClock.cs
@@ -1,6 +1,13 @@
 namespace ITCS3112_Lab1.Contracts;
 
+/// <summary>
+/// Gets the current date.
+/// </summary>
 public interface IClock
 {
+    /// <summary>
+    /// Gets today's date.
+    /// </summary>
+    /// <returns>The current date.</returns>
     DateTime Today();
 }
