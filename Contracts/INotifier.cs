569c0a8316721b4510c121e6711ed298ede2b144
daniel <ddelga12@charlotte.edu>
2026-02-20 01:00:51 -0500

    added xml for iclock

--- Contracts/INotifier.cs
+++ Contracts/INotifier.cs
@@ -2,8 +2,22 @@ using ITCS3112_Lab1.Domain;
 
 namespace ITCS3112_Lab1.Contracts;
 
+/// <summary>
+/// Interface for the notification functionality.
+/// </summary>
 public interface INotifier
 {
+    /// <summary>
+    /// Notifies that an item is due soon.
+    /// </summary>
+    /// <param name="borrower">The borrower.</param>
+    /// <param name="record">The checkout record.</param>
     void DueSoon(Borrower borrower, CheckoutRecord record);
+    
+    /// <summary>
+    /// Notifies that an item is overdue.
+    /// </summary>
+    /// <param name="borrower">The borrower.</param>
+    /// <param name="record">The checkout record.</param>
     void Overdue(Borrower borrower, CheckoutRecord record);
 }
